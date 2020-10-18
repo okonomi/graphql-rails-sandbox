80f8c828615543e0e0739beca504e6c9169222f9
okonomi <okonomi@oknm.jp>
2020-10-18 22:57:41 +0900

    feat: use association loader

--- app/graphql/graphql_rails_sandbox_schema.rb
+++ app/graphql/graphql_rails_sandbox_schema.rb
@@ -8,4 +8,6 @@ class GraphqlRailsSandboxSchema < GraphQL::Schema
 
   # Add built-in connections for pagination
   use GraphQL::Pagination::Connections
+
+  use GraphQL::Batch
 end
--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -6,5 +6,9 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :user, Types::UserType, null: false
+
+    def user
+      Loaders::AssociationLoader.for(Post, :user).load(object)
+    end
   end
 end
